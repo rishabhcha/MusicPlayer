68e637e56b307d1e75b80c50bcbfd48da9574bb8
rishabhcha <rishabhchandel1996@gmail.com>
2017-12-16 13:15:07 +0530

    Initial commit

---
+++ app/src/main/java/com/stare/out/olamusicplayer/MainActivity.java
@@ -0,0 +1,13 @@
+package com.stare.out.olamusicplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
